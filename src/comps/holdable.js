5870be51c25a22eb3ca07f11e155779dbc6297b8
oekshido <joseugarte2262005@gmail.com>
2021-04-21 16:13:03 -0400

    support for touch

--- src/comps/holdable.js
+++ src/comps/holdable.js
@@ -2,21 +2,28 @@ export function holdable(node, duration = 750) {
 	let clicked = false
 	let timeout
 	let triggerPress = true
-	node.addEventListener("mousedown", function() {
+	
+	function onDown() {
 		clicked = true
 		timeout = setTimeout(function () {
 			triggerPress = false
 			node.dispatchEvent(new CustomEvent("hold"))
 		}, duration)
-	})
-	node.addEventListener("mouseup", function() {
+	}
+	
+	function onUp() {
 		if (triggerPress && clicked) {
 			clearTimeout(timeout)
 			node.dispatchEvent(new CustomEvent("press"))
 		}
 		triggerPress = true
 		clicked = false
-	})
+	}
+	
+	node.addEventListener("mousedown", onDown)
+	node.addEventListener("mouseup", onUp)
+	node.addEventListener("touchstart", onDown)
+	node.addEventListener("touchend", onDown)
 	
 	return {
 		destroy() {
