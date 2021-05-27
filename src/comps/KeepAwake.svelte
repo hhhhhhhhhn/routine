6cee5fa36b111010126dcd79d800457df3c26ba0
oekshido <joseugarte2262005@gmail.com>
2021-05-27 11:49:31 -0400

    moved to wakelock api

--- src/comps/KeepAwake.svelte
+++ src/comps/KeepAwake.svelte
@@ -1,12 +1,16 @@
-<!-- svelte-ignore a11y-media-has-caption -->
-<video
-	loop
-	autoplay
-	src="data:video/webm;base64,GkXfo0AgQoaBAUL3gQFC8oEEQvOBCEKCQAR3ZWJtQoeBAkKFgQIYU4BnQI0VSalmQCgq17FAAw9CQE2AQAZ3aGFtbXlXQUAGd2hhbW15RIlACECPQAAAAAAAFlSua0AxrkAu14EBY8WBAZyBACK1nEADdW5khkAFVl9WUDglhohAA1ZQOIOBAeBABrCBCLqBCB9DtnVAIueBAKNAHIEAAIAwAQCdASoIAAgAAUAmJaQAA3AA/vz0AAA="
-/>
+<script>
+	import { onDestroy } from "svelte"
 
-<style>
-	video {
-		opacity: 0;
-	}
-</style>
+	let wakeLock
+
+	try {
+		wakeLock = navigator.wakeLock.request("screen")
+	} catch (err) {}
+
+	onDestroy(async function () {
+		if (wakeLock) {
+			wakeLock = await wakeLock
+			wakeLock.release()
+		}
+	})
+</script>
