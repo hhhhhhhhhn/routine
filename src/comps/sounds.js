7a3dd13e18be086c4171a2bb8f02524b2d2b8990
oekshido <joseugarte2262005@gmail.com>
2021-05-27 10:30:17 -0400

    made beeps stop properly

--- src/comps/sounds.js
+++ src/comps/sounds.js
@@ -12,4 +12,5 @@ export function beep(type = "triangle", duration = 2) {
 	oscilator.start(0)
 	gain.gain
 		.exponentialRampToValueAtTime(0.0001, context.currentTime + duration)
+	oscilator.stop(context.currentTime + duration)
 }
